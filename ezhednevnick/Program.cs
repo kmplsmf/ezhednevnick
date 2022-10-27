b174ad2d73d254c30b63ea0ae83fb58b91d1fce0
kmplsmf <114455197+kmplsmf@users.noreply.github.com>
2022-10-27 19:02:15 +0300

    Update Program.cs

--- ezhednevnick/Program.cs
+++ ezhednevnick/Program.cs
@@ -1,6 +1,8 @@
-﻿int position = 4;
+int position = 4;
 int dt = 0;
-while(true)
+int dt1 = 0;
+DateTime date = new(2017, 6, 2);
+while (true)
 {
     ConsoleKeyInfo key = Console.ReadKey();
     if (key.Key == ConsoleKey.UpArrow)
@@ -22,16 +24,16 @@ while(true)
     else if (key.Key == ConsoleKey.LeftArrow)
     {
         Console.Clear();
-        DateTime date = new(2017, 6, 2);
-        date = date.AddDays(-dt);
+        date = new(2017, 6, 2);
+        date = date.AddDays(-dt1);
         Console.WriteLine(date);
-        dt++;
+        dt1++;
         zametki();
     }
     else if (key.Key == ConsoleKey.RightArrow)
     {
         Console.Clear();
-        DateTime date = new(2017, 6, 2);
+        date = new(2017, 6, 2);
         date = date.AddDays(dt);
         Console.WriteLine(date);
         dt++;
@@ -39,12 +41,37 @@ while(true)
     }
     else if (key.Key == ConsoleKey.Z)
     {
+        dt1 = 0;
         dt = 0;
         Console.Clear();
     }
     else if (key.Key == ConsoleKey.Escape)
     {
         break;
+    }
+    else if (key.Key == ConsoleKey.Enter)
+    {
+        if (position == 3)
+        {
+            Console.Clear();
+            Console.WriteLine(date);
+            Console.WriteLine("---------------------------");
+            variant1();
+        }
+        else if (position == 4)
+        {
+            Console.Clear();
+            Console.WriteLine(date);
+            Console.WriteLine("---------------------------");
+            variant2();
+        }
+        else if (position == 5)
+        {
+            Console.Clear();
+            Console.WriteLine(date);
+            Console.WriteLine("---------------------------");
+            variant3();
+        }
     }
 }
 static void zametki()
@@ -54,4 +81,21 @@ static void zametki()
     Console.WriteLine("    1. Сходить на пары");
     Console.WriteLine("    2. Поспать дома в тёпленькой кроватке");
     Console.WriteLine("    3. Пойти попить пивка");
+}
+static void variant1()
+{
+    Console.WriteLine("вы сходили на пары");
+    Console.WriteLine("интелект +5");
+    Console.WriteLine("усталость +1");
+}
+static void variant2()
+{
+    Console.WriteLine("вы поспали дома в тёпленькой кроватке");
+    Console.WriteLine("интелект -2");
+    Console.WriteLine("усталость -5");
+}
+static void variant3()
+{
+    Console.WriteLine("вы попили пивка");
+    Console.WriteLine("увы тут без циферок, но я могу сказать что ты становишься быдлом и скоро станешь слушать русский подъездный рэп");
 }
